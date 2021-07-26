517139a687db7a699968915fc0d272267921049b
Maxim Babain <makbabain@gmail.com>
2021-07-26 10:38:21 +0300

    Production build

---
+++ js/chunk-2d22d746.5a25bfbc.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,e,t){"use strict";t.r(e);var c=t("7a23"),u={class:"about"},a=Object(c["i"])("h1",null,"This is an about page",-1);function s(n,e){return Object(c["u"])(),Object(c["e"])("div",u,[a])}const i={};i.render=s;e["default"]=i}}]);
+//# sourceMappingURL=chunk-2d22d746.5a25bfbc.js.map
